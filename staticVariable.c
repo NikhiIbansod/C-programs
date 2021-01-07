d8316cba944ac67648d4d9a124c83df18eb7a8c5
Nikhil Bansod <73024614+NikhiIbansod@users.noreply.github.com>
2021-01-07 12:42:17 +0530

    Add files via upload

---
+++ staticVariable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int var;
+ int var1=200;
+ static int var2;
+ static int var3=30;
+ printf("%d",var);
+ printf("\n%d",var1);
+ printf("\n%d\n",var2);
+ printf("%d",var3);
+ return 0;
+}
